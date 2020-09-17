d8de53e0f4b70ead36b8ae60d6c2ea522071d3c9
Heber <hebergonza@gmail.com>
2020-09-17 04:09:48 -0400

    passed all tests in stack.py

---
+++ stack/singly_linked_list.py
@@ -0,0 +1,94 @@
+class Node:
+    def __init__(self, value=None, next_node=None, prev_node=None):
+        self.value = value
+        self.next_node = next_node
+        self.prev_node = prev_node
+
+    def get_value(self):
+        return self.value
+
+    def get_next_node(self):
+        return self.next_node
+
+    def get_prev_node(self):
+        return self.prev_node
+
+    def set_next_node(self, new_next):
+        self.next_node = new_next
+
+    def set_prev_node(self, prev_node):
+        self.prev_node = prev_node
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_to_head(self, value):
+        new_node = Node(value)
+        # if LL is empty
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.set_next_node(self.head)
+            self.head = new_node
+
+    def add_to_tail(self, value):
+        new_node = Node(value)
+        # if LL is empty
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            prev_node = self.tail
+            self.tail.set_next_node(new_node)
+            self.tail = new_node
+            self.tail.set_prev_node(prev_node)
+
+    def remove_head(self):
+        if self.head is None:
+            return None
+        else:
+            ret_value = self.head.get_value()
+            # if LL has one element
+            if self.head == self.tail:
+                self.head = None
+                self.tail = None
+            # if LL has two or more elements
+            else:
+                self.head = self.head.get_next_node()
+            return ret_value
+
+    def remove_tail(self):
+        # empty list?
+        if self.head is None:
+            return None
+        # list with 1 element?
+        else:
+            ret_value = self.tail.get_value()
+            if self.head == self.tail:
+                self.head = None
+                self.tail = None
+            # list with +2 elements?
+            else:
+                self.tail = self.head
+            return ret_value
+
+    def contains(self, value):
+        cur_node = self.head
+        while cur_node is not None:
+            if cur_node.get_value() == value:
+                return True
+        return False
+
+    def get_max(self):
+        cur_node = self.head
+        max_value = self.head.get_value()
+        while cur_node is not None:
+            if max_value < cur_node.get_value():
+                max_value = cur_node.get_value()
+            cur_node = cur_node.get_next_node()
+        return max_value
+
--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,78 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.array = []
+#
+#     def __len__(self):
+#         return self.size
+#
+#     def push(self, value):
+#         self.array.append(value)
+#         self.size = self.size + 1
+#
+#     def pop(self):
+#         ret_val = None
+#
+#         if self.size > 0:
+#             ret_val = self.array[self.size - 1]
+#
+#             self.array.pop()
+#             self.size = self.size - 1
+#
+#         return ret_val
+
+
+import sys
+sys.path.insert(1, '../singly_linked_list/')
+
+
+from singly_linked_list import LinkedList, Node
+
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.linkedList = LinkedList()
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        new_node = Node(value)
+        if self.linkedList.head is None:
+            self.linkedList.head = new_node
+        elif self.linkedList.head is not None and self.linkedList.tail is None:
+            self.linkedList.head.set_next_node(new_node)
+            self.linkedList.tail = new_node
+        else:
+            self.linkedList.add_to_tail(value)
+        self.size = self.size + 1
 
     def pop(self):
-        pass
+        if self.linkedList.head is None:
+            self.size = 0
+            return None
+        else:
+            ret_value = None
+            if self.linkedList.head is not None and self.linkedList.tail is None:
+                ret_value = self.linkedList.head.get_value()
+                self.linkedList.head = None
+            elif self.size == 2:
+                ret_value = self.linkedList.tail.get_value()
+                self.linkedList.tail = None
+                self.linkedList.head.set_next_node(None)
+            elif self.size > 2:
+                ret_value = self.linkedList.tail.get_value()
+                self.linkedList.tail = self.linkedList.tail.get_prev_node()
+            self.size = self.size - 1
+            return ret_value
+
+
+# the difference between using an array and a linked list is that the array stores all the
+# elements together side-by-side and the linked list doesn't.
+# the list has built in methods that facilitate these operations. 
