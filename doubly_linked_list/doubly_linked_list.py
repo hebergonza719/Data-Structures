bab86c079bf4302f6017b6893ccaf89a548049f7
Heber <hebergonza@gmail.com>
2020-09-19 16:33:33 -0400

    created add_to_head and delete

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -4,10 +4,19 @@ as well as its next node in the List.
 """
 class ListNode:
     def __init__(self, value, prev=None, next=None):
-        self.prev = prev
         self.value = value
+        self.prev = prev
         self.next = next
-            
+
+    # updates next and previous pointer for those next to the node
+    def delete(self):
+        # if there is a previous pointer - not for head
+        if self.prev:
+            self.next.prev = self.prev
+        # if there is a next pointer - not for tail
+        if self.next:
+            self.prev.next = self.next
+
 """
 Our doubly-linked list class. It holds references to 
 the list's head and tail nodes.
@@ -27,7 +36,19 @@ class DoublyLinkedList:
     the old head node's previous pointer accordingly.
     """
     def add_to_head(self, value):
-        pass
+        # create new_node
+        new_node = ListNode(value)
+        # 1. add to empty DLL
+        if self.head is None:
+            self.head == new_node
+            self.tail == new_node
+        # 2. add to nonempty
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        # update length
+        self.length += 1
         
     """
     Removes the List's current head node, making the
@@ -58,7 +79,10 @@ class DoublyLinkedList:
     List and inserts it as the new head node of the List.
     """
     def move_to_front(self, node):
-        pass
+        if node is self.head:
+            return None
+        self.delete(node)
+        self.add_to_head(node.value)
         
     """
     Removes the input node from its current spot in the 
@@ -72,7 +96,31 @@ class DoublyLinkedList:
     order of the other elements of the List.
     """
     def delete(self, node):
-        pass
+        # don't need to return value
+
+        # Do need to update head, tail
+        # list is empty
+        if self.head is None:
+            return None
+        # list has one element
+        elif self.head is self.tail:
+            self.head = None
+            self.tail = None
+        # list has 2 or more elements
+        elif node is self.head:
+            self.head = node.next
+            # self.head.prev = None
+            node.delete()
+        elif node is self.tail:
+            self.tail = node.prev
+            # self.tail.next = None
+            node.delete()
+        else:
+            # remove from middle
+            node.delete()
+
+        self.length -= 1
+
 
     """
     Finds and returns the maximum value of all the nodes 
@@ -80,3 +128,9 @@ class DoublyLinkedList:
     """
     def get_max(self):
         pass
+
+
+example = DoublyLinkedList()
+print(example.length)
+example.add_to_head(1)
+print(example.length)
