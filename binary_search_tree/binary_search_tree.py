4394efd21154452b5930856e78211ff25c22c2a2
Heber <hebergonza@gmail.com>
2020-09-22 21:56:44 -0400

    finished insert and contains

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,12 +17,37 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if value < self.value:
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        if value >= self.value:
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        print("value of root", self.value)
+        if self.value is target:
+            return True
+        elif self.value > target and self.left:
+            if self.left.value is target:
+                return True
+            else:
+                # self.left.contains(target)
+                return self.left.contains(target)
+        elif self.value < target and self.right:
+            if self.right.value is target:
+                return True
+            else:
+                # self.right.contains(target)
+                return self.right.contains(target)
+        return False
+
 
     # Return the maximum value found in the tree
     def get_max(self):
@@ -73,13 +98,15 @@ bst.insert(3)
 bst.insert(4)
 bst.insert(2)
 
-bst.bft_print()
-bst.dft_print()
+print(bst.contains(2))
 
-print("elegant methods")
-print("pre order")
-bst.pre_order_dft()
-print("in order")
-bst.in_order_dft()
-print("post order")
-bst.post_order_dft()  
+# bst.bft_print()
+# bst.dft_print()
+
+# print("elegant methods")
+# print("pre order")
+# bst.pre_order_dft()
+# print("in order")
+# bst.in_order_dft()
+# print("post order")
+# bst.post_order_dft()  
